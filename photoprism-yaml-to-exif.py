97c9d0a7bf209e06eb732d4e38144f78d3be9305
Cheuk Yin Ng <chucksys88@gmail.com>
2024-12-22 02:23:04 -0800

    feat: add datetime defaults
    if one of them don't exist then default to the first month and/or the
    first day. if the year doesn't exist, then don't do it at all.

--- photoprism-yaml-to-exif.py
+++ photoprism-yaml-to-exif.py
@@ -157,7 +157,18 @@ def do_the_file(eft: exiftool.ExifToolHelper, args: argparse.Namespace, sidecar_
                                       day=yaml_sidecar['Day'])
                     tags_to_edit['DateTimeOriginal'] = d.isoformat()
                 except ValueError:
-                    logger.error(f'Bad date value {yaml_sidecar["Year"]}-{yaml_sidecar["Month"]}-{yaml_sidecar["Day"]}')
+                    d = datetime.date(year=1, month=1, day=1)
+                    if yaml_sidecar['Year'] > 0:
+                        d.year = yaml_sidecar['Year']
+                        if yaml_sidecar['Month'] > 0:
+                            d.month = yaml_sidecar['Month']
+                        if yaml_sidecar['Day'] > 0:
+                            d.day = yaml_sidecar['Day']
+
+                        tags_to_edit['DateTimeOriginal'] = d.isoformat()
+                        logger.warning(f'Incomplete date value; using {d.isoformat()} instead')
+                    else:
+                        logger.error('Year is negative')
 
     if args.details and 'Details' in yaml_sidecar:
         if 'Keywords' in yaml_sidecar['Details']:
